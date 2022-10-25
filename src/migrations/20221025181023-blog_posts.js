fb43039041b31997635a502bfa0d1aa6af6e5466
BraKa <brayan.santosrosa@gmail.com>
2022-10-25 15:52:59 -0300

    Requisito 01 concluido

---
+++ src/migrations/20221025181023-blog_posts.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: Sequelize.STRING,
+      content: Sequelize.STRING,
+      user_id: {
+       type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdated: 'CASCADE',
+        ondelete: 'CASCADE',
+      },
+      published: Sequelize.DATE,
+      updated: Sequelize.DATE,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
