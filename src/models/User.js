acf9c76e6363de1a4b7d0eec5f998e6bb8760592
BraKa <brayan.santosrosa@gmail.com>
2022-10-25 16:04:00 -0300

    Requisito 02 concluido

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        displayName: DataTypes.STRING,
+        email: {
+           type: DataTypes.STRING,
+           unique: true,
+        },
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+    }, {
+        underscored:true,
+        timestamps: false,
+    });
+    return User;
+};
